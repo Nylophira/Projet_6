a42affc625d5dcc2b411d8103b279de35d377639
Nylophira <121953951+Nylophira@users.noreply.github.com>
2023-08-25 09:46:29 +0200

    modif suite soutenance

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,12 @@
 import imageFooter from "../assets/FooterD.png";
-import imageFooterS from "../assets/FooterS.png";
 import '../style/footer.css';
 
 function Footer() {
     return (
-      <div className="monFooter">
-        <img className="Ordi" src={imageFooter} alt="Logo de Kasa" />
-        <img className="mobile" src={imageFooterS} alt="Logo de Kasa" />
-      </div>
+      <footer className="monFooter">
+        <img src={imageFooter} alt="Logo de Kasa" />
+        <p>© 2020 Kasa. All rights reserved</p>
+      </footer>
     );
   }
   
